d3b25965229d598e178b2a3a8508fdda0d1f727a
manoskir <em.kiriakakis@gmail.com>
2024-05-20 23:16:52 +0300

    Creates function to add a new block, hash a block and return last block

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,34 @@
-
+from time import time
+import hashlib
+import json
 
 class Blockchain(object):
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
-	def new_block(self):
-		# Creates a new Block and adds it to the chain
-		pass
+	def new_block(self, proof, pevious_hash=None):
+		'''
+		Creates a new Block and adds it to the chain
+		:param proof: <int> The proof given by the proof of work algo
+		:param previous_hash: (Optional) <str> Hash of previous block
+		:return: <dict> new block
+		'''
+
+		block = {
+			'index': len(self.chain) + 1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.chain[-1])
+		}
+
+		#resets the list of current transactions
+		self.current_transactions = []
+
+		self.chain.append(block)
+		return block
+
 
 	def new_transaction(self, sender, recipent, amount):
 		# Adds a new transaction to the list of transactions
@@ -21,13 +42,13 @@ class Blockchain(object):
 
 	@staticmethod
 	def hash(block):
-		# Hashes a block
-		pass
+		# Creates a sha-256 hash of a block
+		# The dictionary must be orderd, or we'll have inconsistencies and this is why we use the json dumps
+		block_string = json.dumps(block, sort_keys=True).encode()
+
+		return hashlib.sha256(block_string).hexdigest()
 
 	@property
 	def last_block(self):
 		#returns the last block in the chain
-		pass
-
-
-		
+		return self.chain[-1]
